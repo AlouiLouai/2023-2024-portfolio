70e9fc25a9028c1b9bb86c41e4905bff44f96ecc
louaialoui1993 <louaialoui1993@gmail.com>
2025-04-24 07:56:20 +0000

    using firebase

--- app/Home/TypeWriter.tsx
+++ app/Home/TypeWriter.tsx
@@ -1,22 +1,41 @@
 import Typewriter from "typewriter-effect";
 
 function TypeWriter() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Software engineer",
-          "Full Stack javascript engineer",
-          "python developer",
-          "A dreamer to become one of the brightest engineer in the world!",
-          "And Why not making a radical contribution to wrold technologies"
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
-  );
+    return (
+        <Typewriter
+            options={{
+                strings: [
+                    "Software engineer",
+                    "Full Stack javascript engineer",
+                    "python developer",
+                    "A dreamer to become one of the brightest engineer in the world!",
+                    "And Why not making a radical contribution to wrold technologies"
+                ],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+                wrapperClassName: "typewriter-wrapper",
+                cursorClassName: "typewriter-cursor",
+                
+                
+                
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .callFunction(() => {
+                  const elements = document.getElementsByClassName('typewriter-wrapper');
+                for (let i = 0; i < elements.length; i++) {
+                  elements[i].classList.add("animate-text");
+                }
+                
+                })
+                .pauseFor(2500)
+                .deleteAll()
+                
+              
+            }}
+        />
+    );
 }
 
 export default TypeWriter;
--- app/components/Particle.tsx
+++ app/components/Particle.tsx
@@ -1,50 +1,80 @@
 import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
 
 function Particle() {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
   return (
     <Particles
-      params={{
-        particles: {
-          number: {
-            value: 160,
-            density: {
-              enable: true,
-              value_area: 1500,
-            },
-          },
-          line_linked: {
-            enable: false,
-            opacity: 0.03,
-          },
-          move: {
-            direction: "right",
-            speed: 0.05,
-          },
-          size: {
-            value: 1,
-          },
-          opacity: {
-            anim: {
-              enable: true,
-              speed: 1,
-              opacity_min: 0.05,
-            },
-          },
-        },
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        fpsLimit: 120,
         interactivity: {
           events: {
-            onclick: {
+            onClick: {
               enable: true,
               mode: "push",
             },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
           },
           modes: {
             push: {
-              particles_nb: 1,
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
             },
           },
         },
-        retina_detect: true,
+        particles: {
+          color: {
+            value: ["#BD10E0", "#B8E986", "#50E3C2", "#FFD300", "#E86363"],
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: false,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 1.5,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 900,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+            },
+          },
       }}
     />
   );
